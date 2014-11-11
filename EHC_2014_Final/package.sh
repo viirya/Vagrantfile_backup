696843eb3e8d2fddeda27c2e698b77172774f9f1
Liang-Chi Hsieh <viirya@gmail.com>
2014-11-11 15:36:29 +0800

    init import.

---
+++ EHC_2014_Final/package.sh
@@ -0,0 +1,4 @@
+vagrant package --base EHC_2014_Final_vm1_1401390970338_47154 --output centos-64-x64-vbox4312-hadoop-vm1-newbase6.box 
+vagrant package --base EHC_2014_Final_vm2_1401391113550_99797 --output centos-64-x64-vbox4312-hadoop-vm2-newbase6.box 
+vagrant package --base EHC_2014_Final_vm3_1401391262200_81082 --output centos-64-x64-vbox4312-hadoop-vm3-newbase6.box 
+vagrant package --base EHC_2014_Final_vm4_1401391412847_23975 --output centos-64-x64-vbox4312-hadoop-vm4-newbase6.box 
---
+++ EHC_2014_Final/run.sh
@@ -0,0 +1,4 @@
+vagrant box add centos64-hadoop-vm1-newbase6 centos-64-x64-vbox4312-hadoop-vm1-newbase6.box
+vagrant box add centos64-hadoop-vm2-newbase6 centos-64-x64-vbox4312-hadoop-vm2-newbase6.box
+vagrant box add centos64-hadoop-vm3-newbase6 centos-64-x64-vbox4312-hadoop-vm3-newbase6.box
+vagrant box add centos64-hadoop-vm4-newbase6 centos-64-x64-vbox4312-hadoop-vm4-newbase6.box
